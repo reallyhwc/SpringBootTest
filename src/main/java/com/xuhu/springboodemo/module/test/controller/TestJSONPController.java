4a641026e2ff870225b7069433072ea1c5e9aa75
xuhu <reallyhwc@foxmail.com>
2021-03-29 20:15:59 +0800

    提供一个jsonP接口供他人调试跨域使用

--- src/main/java/com/xuhu/springboodemo/module/test/controller/TestJSONPController.java
+++ src/main/java/com/xuhu/springboodemo/module/test/controller/TestJSONPController.java
@@ -2,11 +2,9 @@ package com.xuhu.springboodemo.module.test.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.xuhu.springboodemo.core.domain.ResultMsg;
-import com.xuhu.springboodemo.module.test.domain.TestCity;
 import com.xuhu.springboodemo.module.test.service.TestCityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,12 +29,8 @@ public class TestJSONPController {
 
         ResultMsg msg = ResultMsg.ok("成功", testCityService.getCityList().get(0));
 
-        String oldAns = callback+"({result:'jsonp'})";
         String newAns = callback + String.format("(%s)", JSONObject.toJSONString(msg));
-
-        log.error(oldAns);
         log.error(newAns);
-
         return newAns;
     }
 }
